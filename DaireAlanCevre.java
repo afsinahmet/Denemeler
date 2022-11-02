28e19ad3d704a0aff161a28578dfad06498c96e6
Ahmet Afşin <108630442+afsinahmet@users.noreply.github.com>
2022-11-02 17:46:02 +0300

    DaireAlanCevreHesaplama

---
+++ DaireAlanCevre.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class DaireAlanCevre {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+
+        double r,alan,cevre,pi=3.14;
+
+        System.out.print("Yarıcapı giriniz : ");
+        r=inp.nextDouble();
+
+        alan=pi*r*r;
+        cevre=2*pi*r;
+
+        System.out.println("Dairenin Alanı: " + alan);
+        System.out.print("Dairenin Çevresi : "+ cevre );
+
+
+
+
+
+
+    }
+
+    }
