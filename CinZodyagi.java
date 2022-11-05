5f49d16b578b5bed0013ca6e30eed0881832b092
Ahmet Afşin <108630442+afsinahmet@users.noreply.github.com>
2022-11-06 00:08:52 +0300

    Çiz Zodyak Burcunu Hesaplama

---
+++ CinZodyagi.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class CinZodyagi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int year, result;
+        String zodyak = "";
+
+        System.out.print("Doğum yılınızı giriniz: ");
+        year = inp.nextInt();
+        result = year % 12;
+
+        switch (result) {
+            case 0:
+                zodyak = "Maymun";
+                break;
+            case 1:
+                zodyak = "Horoz";
+                break;
+            case 2:
+                zodyak = "Köpek";
+                break;
+            case 3:
+                zodyak = "Domuz";
+                break;
+            case 4:
+                zodyak = "Fare";
+                break;
+            case 5:
+                zodyak = "Öküz";
+                break;
+            case 6:
+                zodyak = "Kaplan";
+                break;
+            case 7:
+                zodyak = "Tavşan";
+                break;
+            case 8:
+                zodyak = "Ejderha";
+                break;
+            case 9:
+                zodyak = "Yılan";
+                break;
+            case 10:
+                zodyak = "At";
+                break;
+            case 11:
+                zodyak = "Koyun";
+                break;
+
+
+        }
+
+        System.out.print("Çin Zodyağı Burcunuz: " + zodyak);
+    }
+}
